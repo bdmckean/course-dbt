af482243756b9c82a4e485fefbc7bd6a2c2848d3
Brian McKean <bdmckean@gmail.com>
2024-10-26 18:02:33 +0000

    week3 md

--- greenery/models/intermediate/_int_product_events.sql
+++ greenery/models/intermediate/_int_product_events.sql
@@ -1,24 +1,33 @@
  {{
-
     config(
         materialized='view'
     )
 }}
 
 
-with sessions as
+with product_sessions as
 (
     select * 
     from {{ ref('_stg_postgres_events') }}
+    where product_id is not null
+),
+purchase as
+(
+    select session_id
+    from {{ ref('_stg_postgres_events') }}
+    where event_type = 'checkout'
 )
 
 SELECT 
-    session_id
-    , product_id
+    s.session_id
+    , s.product_id
     , {{ has_item('event_type', 'page_view') }}
     , {{ has_item('event_type','add_to_cart') }} 
-    , {{ has_item('event_type','product_shipped') }}
-    , {{ has_item('event_type','checkout') }}
-FROM sessions
-GROUP BY 1,2
-ORDER BY product_id, session_id
+    , case when p.session_id is not null then true
+        else false end as has_purchase
+
+FROM product_sessions s
+full outer join purchase p
+on s.session_id = p.session_id
+GROUP BY s.session_id, s.product_id, p.session_id
+ORDER BY s.product_id, s.session_id
--- greenery/models/marts/product/fact_product_conversion_rate.sql
+++ greenery/models/marts/product/fact_product_conversion_rate.sql
@@ -6,23 +6,16 @@ with product_events as
     select
     *
     from {{ ref('_int_product_events') }}
-),
-purchases AS (
-    SELECT 
-        session_id
-        , product_id
-        , MAX(CASE WHEN has_checkout = true THEN 1 ELSE 0 END) as has_purchase
-    FROM product_events
-    GROUP BY 1,2
 )
+
 
 SELECT 
     product_id
     , COUNT(DISTINCT session_id) as total_sessions
-    , SUM(has_purchase) as sessions_with_purchase
-    , ROUND(CAST(SUM(has_purchase) AS FLOAT) / 
+    , COUNT_IF(has_purchase = true) as sessions_with_purchase
+    , ROUND(CAST(count_if(has_purchase = true) AS FLOAT) / 
           CAST(COUNT(DISTINCT session_id) AS FLOAT), 3) as purchase_ratio
-FROM purchases
+FROM product_events
 GROUP BY product_id
 ORDER BY product_id
 
